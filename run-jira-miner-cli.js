374ff1dc379284d365eb19909bdf8cfd11679dbf
austincunningham <austincunningham@hotmail.com>
2017-03-19 21:31:53 +0000

    added project input for cli

--- run-jira-miner-cli.js
+++ run-jira-miner-cli.js
@@ -1,9 +1,10 @@
 /**
  * Created by austin on 16/03/2017.
- */
+ */// jscs:ignore
 
 'use strict';
 var prompt = require('prompt');
+
 //var sys = require('sys'); deprecated
 //var util = require('util'); deprecated
 let exec = require('child_process').exec;
@@ -13,17 +14,19 @@ let child;
 
 prompt.start();
 
-// Get two properties from the user: username and email
-
-prompt.get(['username', 'password'], function (err, result) {
+// Get three properties from the user: username , password and url
+console.log('Enter username, password and url e.g. "issues.jboss.org"');
+prompt.get(['username', 'password', 'url', 'project'], function (err, result) {
   // Log the results.
 
   console.log('Command-line input received:');
-  console.log('  username: ' + result.username);
-  console.log('  password: ' + result.password);
+  console.log(' username: ' + result.username);
+  console.log(' password: ' + result.password);
+  console.log(' url:' + result.url);
 
   // execute jira-miner target to point at the source
-  child = exec('jira-miner target https://issues.jboss.org --user ' + result.username + ' --password ' + result.password, function (error, stdout, stderr) {
+  child = exec('jira-miner target https://' + result.url + ' --user ' + result.username +
+      ' --password ' + result.password, function (error, stdout, stderr) {
     console.log('stdout: ' + stdout);
     console.log('stderr: ' + stderr);
     if (error !== null) {
@@ -31,17 +34,20 @@ prompt.get(['username', 'password'], function (err, result) {
     }
   });
 
+  console.log('Enter project name e.g. RAINCATCH');
   // pull data from the target with populate, to populate local loki db
-  child = exec('jira-miner populate "project in (RAINCATCH)"', function (error, stdout, stderr) {
+  child = exec('jira-miner populate "project in (' + result.project + ')"', function (error, stdout, stderr) {
     console.log('stdout: ' + stdout);
     console.log('stderr: ' + stderr);
     if (error !== null) {
       console.log('exec error: ' + error);
     }
   });
+
 
   // use jira-miner query to access a local search file
-  child = exec('jira-miner query search --key=RAINCATCH-623 --json', function (error, stdout, stderr) {
+  child = exec('jira-miner query search --key=RAINCATCH-623 --json',
+  function (error, stdout, stderr) {
     console.log('stdout: ' + stdout);
     console.log('stderr: ' + stderr);
     if (error !== null) {
@@ -49,6 +55,7 @@ prompt.get(['username', 'password'], function (err, result) {
     }
   });
 });
+
 
 // a more concise way to run jira-miner populate (uses deprecated components of node)
 /*
