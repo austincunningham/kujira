1eadc60a9848147fba6bfc164cf725582752ed94
austincunningham <austincunningham@hotmail.com>
2017-03-19 14:27:32 +0000

    added command prompt input for username and password

--- run-jira-miner-cli.js
+++ run-jira-miner-cli.js
@@ -3,37 +3,51 @@
  */
 
 'use strict';
-
+var prompt = require('prompt');
 //var sys = require('sys'); deprecated
 //var util = require('util'); deprecated
 let exec = require('child_process').exec;
 let child;
 
-// execute jira-miner target to point at the source
-child = exec('jira-miner target https://issues.jboss.org --user usernamehere --password passwordhere', function (error, stdout, stderr) {
-  console.log('stdout: ' + stdout);
-  console.log('stderr: ' + stderr);
-  if (error !== null) {
-    console.log('exec error: ' + error);
-  }
-});
+// Start the prompt
 
-// pull data from the target with populate, to populate local loki db
-child = exec('jira-miner populate "project in (RAINCATCH)"', function (error, stdout, stderr) {
-  console.log('stdout: ' + stdout);
-  console.log('stderr: ' + stderr);
-  if (error !== null) {
-    console.log('exec error: ' + error);
-  }
-});
+prompt.start();
 
-// use jira-miner query to access a local search file
-child = exec('jira-miner query search --key=RAINCATCH-623 --json', function (error, stdout, stderr) {
-  console.log('stdout: ' + stdout);
-  console.log('stderr: ' + stderr);
-  if (error !== null) {
-    console.log('exec error: ' + error);
-  }
+// Get two properties from the user: username and email
+
+prompt.get(['username', 'password'], function (err, result) {
+  // Log the results.
+
+  console.log('Command-line input received:');
+  console.log('  username: ' + result.username);
+  console.log('  password: ' + result.password);
+
+  // execute jira-miner target to point at the source
+  child = exec('jira-miner target https://issues.jboss.org --user ' + result.username + ' --password ' + result.password, function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+  });
+
+  // pull data from the target with populate, to populate local loki db
+  child = exec('jira-miner populate "project in (RAINCATCH)"', function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+  });
+
+  // use jira-miner query to access a local search file
+  child = exec('jira-miner query search --key=RAINCATCH-623 --json', function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+  });
 });
 
 // a more concise way to run jira-miner populate (uses deprecated components of node)
