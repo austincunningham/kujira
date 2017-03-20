25b4cdb2df032cda3c2c844e53245107983b084b
austincunningham <austincunningham@hotmail.com>
2017-03-20 15:26:52 +0000

    remove deprecated calls

--- run-jira-miner-cli.js
+++ run-jira-miner-cli.js
@@ -4,25 +4,24 @@
 
 'use strict';
 var prompt = require('prompt');
-
-//var sys = require('sys'); deprecated
-//var util = require('util'); deprecated
-let exec = require('child_process').exec;
-let child;
+var exec = require('child_process').exec;
+var child;
 
 // Start the prompt
 
 prompt.start();
 
-// Get three properties from the user: username , password and url
+// Get three properties from the user: username , password and url and project
 console.log('Enter username, password and url e.g. "issues.jboss.org"');
 prompt.get(['username', 'password', 'url', 'project'], function (err, result) {
+
   // Log the results.
 
   console.log('Command-line input received:');
   console.log(' username: ' + result.username);
-  console.log(' password: ' + result.password);
-  console.log(' url:' + result.url);
+  console.log(' password: ********');
+  console.log(' url: http://' + result.url);
+  console.log(' project: ' + result.project);
 
   // execute jira-miner target to point at the source
   child = exec('jira-miner target https://' + result.url + ' --user ' + result.username +
@@ -34,9 +33,10 @@ prompt.get(['username', 'password', 'url', 'project'], function (err, result) {
     }
   });
 
-  console.log('Enter project name e.g. RAINCATCH');
+
   // pull data from the target with populate, to populate local loki db
-  child = exec('jira-miner populate "project in (' + result.project + ')"', function (error, stdout, stderr) {
+  child = exec('jira-miner populate "project in (' + result.project + ')"',
+  function (error, stdout, stderr) {
     console.log('stdout: ' + stdout);
     console.log('stderr: ' + stderr);
     if (error !== null) {
@@ -56,11 +56,4 @@ prompt.get(['username', 'password', 'url', 'project'], function (err, result) {
   });
 });
 
-
-// a more concise way to run jira-miner populate (uses deprecated components of node)
-/*
-function puts(error, stdout, stderr) { sys.puts(stdout); }
-
-exec('jira-miner populate "project in RAINCATCH"', puts);
-*/
 
