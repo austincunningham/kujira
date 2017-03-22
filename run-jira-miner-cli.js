46cad1d0e89cfcc3bc2e546a953a39e4aa9bdaea
austincunningham <austincunningham@hotmail.com>
2017-03-22 18:32:57 +0000

    added promises

--- run-jira-miner-cli.js
+++ run-jira-miner-cli.js
@@ -8,39 +8,39 @@ var prompt = require('prompt');
 var exec = require('child_process').exec;
 var child;
 
-//validation for propmpt see https://www.npmjs.com/package/prompt
-var schema ={
+//validation for prompt see https://www.npmjs.com/package/prompt
+var schema = {
   properties: {
     username: {
       description: 'Enter your Jira Username',
       pattern: /^[a-zA-Z0-9._-]+$/,
       message: 'Username must be characters, numbers, dots, underscores and dashes',
-      required: true
+      required: true,
     },
     password: {
       description: 'Enter your Jira password',
       hidden: true,
       replace: '*',
-      required: true
+      required: true,
     },
     url: {
       description: 'Enter Jira URL e.g. "issues.jboss.org"',
       pattern: /^[a-zA-Z0-9._-]+$/,
       message: 'url must be a valid',
-      required: true
+      required: true,
     },
     project: {
       description: 'Enter the project name e.g. "RHMAP or RAINCATCH"',
       pattern: /^[a-zA-Z0-9._-]+$/,
       message: 'Must be a valid project',
-      required: true
+      required: true,
     },
     format: {
       description: 'Enter format either json or csv ,enter for default tsv',
       pattern: /^[a-z]+$/,
-      required: false
-    }
-  }
+      required: false,
+    },
+  },
 };
 
 // Start the prompt
@@ -49,9 +49,9 @@ prompt.start();
 
 // Get three properties from the user: username , password and url and project
 console.log('Enter username, password and url e.g. "issues.jboss.org"');
-//prompt.get(['username', 'password', 'url', 'project'], function (err, result) {
-prompt.get(schema, function(err, result){
+prompt.get(schema, function (err, result) {
 
+  //prompt.get(['username', 'password', 'url', 'project'], function (err, result) {
   // Log the results.
 
   console.log('Command-line input received:');
@@ -68,22 +68,16 @@ prompt.get(schema, function(err, result){
     if (error !== null) {
       console.log('exec error: ' + error);
     }
-  });
-
-
-  // pull data from the target with populate, to populate local loki db
-  child = exec('jira-miner populate "project in (' + result.project + ')"',
+    // pull data from the target with populate, to populate local loki db
+  }).then = exec('jira-miner populate "project in (' + result.project + ')"',
   function (error, stdout, stderr) {
     console.log('stdout: ' + stdout);
     console.log('stderr: ' + stderr);
     if (error !== null) {
       console.log('exec error: ' + error);
     }
-  });
-
-
-  // use jira-miner query to access a local search file
-  child = exec('jira-miner query search --key=RAINCATCH-623 --'+ result.format,
+    // use jira-miner query to access a local search file
+  }).then = exec('jira-miner query search --key=RAINCATCH-623 --' + result.format,
   function (error, stdout, stderr) {
     console.log('stdout: ' + stdout);
     console.log('stderr: ' + stderr);
@@ -92,5 +86,4 @@ prompt.get(schema, function(err, result){
     }
   });
 });
-
 
