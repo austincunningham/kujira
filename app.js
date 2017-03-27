8f705d51bba49c713de7066c9f5415c3675d9b1f
Austin Cunningham <austincunningham@hotmail.com>
2017-03-27 12:55:35 +0100

    Revert "Fh 3368"

---
+++ app.js
@@ -0,0 +1,16 @@
+/**
+ * Created by acunningham on 20/03/17.
+ */
+'use strict';
+var prompt = require('prompt');
+
+var populate = require('./populate.js');
+
+var target = require('./target.js');
+var query = require('./query.js');
+
+
+target();
+prompt.stop();
+//populate();
+//query();
