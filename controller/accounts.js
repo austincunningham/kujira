dad336f4a87e9e3e08ddb6322a3fbd8d8da7b9d1
austincunningham <austincunningham@hotmail.com>
2017-04-02 17:29:22 +0100

    basic login checks jira-mirer for a successful response and renders home page

--- controller/accounts.js
+++ controller/accounts.js
@@ -11,7 +11,33 @@ let child;
 
 //jira-miner target takes json input url,username and password
 router.get('/', function (req, res) {
-  res.render('index');
+  res.render('index',{title: 'Welcome to Kujira'});
+});
+
+router.get('/login', function(req, res){
+  res.render('login',{
+    title: 'Login to Kujira'
+  });
+});
+
+router.get('/home', function (req, res) {
+  res.render('home',{
+    title: 'Kujira Home'
+  });
+});
+
+router.post('/login', function(req, res){
+  child = exec('jira-miner target https://' + req.body.url + ' --user ' + req.body.username +
+      ' --password ' + req.body.password, function (error, stdout, stderr) {
+    if (stdout.indexOf('Successfully targeted JIRA') >= 0 ){
+      res.render('home',{title:'Kujira Home'});
+    }
+    //res.status(200).json(stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+  });
 });
 
 module.exports = router;
