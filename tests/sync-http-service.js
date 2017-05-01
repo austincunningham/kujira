daedd4a5adf2867a7d98267dbcb94436d5787d4f
austincunningham <austincunningham@hotmail.com>
2017-05-01 13:31:04 +0100

    updated other endpoints

--- tests/sync-http-service.js
+++ tests/sync-http-service.js
@@ -12,6 +12,7 @@ class SyncHttpService {
     this.baseUrl = baseUrl;
   }
 
+  //checking if the page has 'Kujira Home' for successful login
   setAuth(url, user) {
     const res = request('POST', this.baseUrl + url, {json: user});
     //will return 200/2xx even if fails as not using standard http auth
@@ -22,16 +23,17 @@ class SyncHttpService {
     return false;
   }
 
+  //checking if the page has 'Welcome to Kujira' for successful logout
   clearAuth(url) {
     const res = request('GET', this.baseUrl + url);
     if(res.body.indexOf('Welcome to Kujira') >=0){
       return true;
     }
   }
-
+  //not used yet
   get(url) {
     var returnedObj = null;
-    var res = request('GET', this.baseUrl + url, { headers: this.authHeadder });
+    var res = request('GET', this.baseUrl + url);
     if (res.statusCode < 300) {
       returnedObj = JSON.parse(res.getBody('utf8'));
     }
@@ -41,7 +43,7 @@ class SyncHttpService {
 
   post(url, obj) {
     var returnedObj = null;
-    var res = request('POST', this.baseUrl + url, { json: obj, headers: this.authHeadder });
+    var res = request('POST', this.baseUrl + url, { json: obj });
     if (res.statusCode < 300) {
       returnedObj = JSON.parse(res.getBody('utf8'));
     }
