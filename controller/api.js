481dff81e0e18e3f83f80902d0776b33135b878b
austincunningham <austincunningham@hotmail.com>
2017-05-17 21:56:14 +0100

    refactor api to return correct http status codes

--- controller/api.js
+++ controller/api.js
@@ -15,9 +15,11 @@ router.post('/target', function (req, res) {
   child = exec('jira-miner target https://' + req.body.url + ' --user ' + req.body.username +
       ' --password ' + req.body.password,{maxBuffer: 1024 * 20000}, function (error, stdout, stderr) {
     res.status(200).json(stdout);
-    console.log('stderr: ' + stderr);
+    if (stderr !== null){
+      res.status(400).json(stderr);
+    }
     if (error !== null) {
-      console.log('exec error: ' + error);
+      res.status(500).json(error);
     }
   });
 });
@@ -27,30 +29,30 @@ router.post('/populate', function (req, res) {
   // execute jira-miner target to point at the source
   child = exec('jira-miner populate "project in (' + req.body.project + ')"',{maxBuffer: 1024 * 20000}, function (error, stdout, stderr) {
     res.status(200).json(stdout);
-    console.log('stderr: ' + stderr);
+    if (stderr !== null){
+      res.status(400).json(stderr);
+    }
     if (error !== null) {
-      console.log('exec error: ' + error);
+      res.status(500).json(error);
     }
   });
 });
 
 // jira-miner query takes in an array of field value objects
 router.post('/query', function (req, res){
-  console.log(req.body);
   let searchString = '';
   for (let i = 0; i < req.body.length; i++){
     searchString += '--' + req.body[i].field +'='+ req.body[i].value +' ';
-    console.log(searchString);
   }
-  console.log('jira-miner query search ' + searchString + '--json');
   child = exec('jira-miner query search ' + searchString + '--json',{maxBuffer: 1024 * 20000}, function (error, stdout, stderr) {
     stdout = JSON.parse(stdout);
     res.status(200).json(stdout);
-    console.log('stderr: ' + stderr);
-    if (error !== null) {
-      console.log('exec error: ' + error);
+    if (stderr !== null){
+      res.status(400).json(stderr);
     }
-
+    if (error !== null) {
+      res.status(500).json(error);
+    }
   });
 });
 
