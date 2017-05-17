8343c8771ea4e10a5499c731a0635fd2393a6476
austincunningham <austincunningham@hotmail.com>
2017-05-17 22:19:51 +0100

    removed comments and setup to return status codes

--- controller/api.js
+++ controller/api.js
@@ -8,33 +8,50 @@ const express = require('express');
 const router = express.Router();
 const exec = require('child_process').exec;
 let child;
+let statusCode;
+let resValue;
 
 //jira-miner target takes json input url,username and password
 router.post('/target', function (req, res) {
   // execute jira-miner target to point at the source
   child = exec('jira-miner target https://' + req.body.url + ' --user ' + req.body.username +
       ' --password ' + req.body.password,{maxBuffer: 1024 * 20000}, function (error, stdout, stderr) {
-    res.status(200).json(stdout);
     if (stderr !== null){
-      res.status(400).json(stderr);
+      statusCode = 400;
+      resValue = stderr;
     }
     if (error !== null) {
-      res.status(500).json(error);
+      statusCode = 500;
+      resValue = error;
+
     }
+    if (stdout !== null) {
+      statusCode = 200;
+      resValue = stdout;
+    }
+    res.status(statusCode).json(resValue);
   });
 });
 
 //jira-miner populate takes json input project
 router.post('/populate', function (req, res) {
   // execute jira-miner target to point at the source
+
   child = exec('jira-miner populate "project in (' + req.body.project + ')"',{maxBuffer: 1024 * 20000}, function (error, stdout, stderr) {
-    res.status(200).json(stdout);
     if (stderr !== null){
-      res.status(400).json(stderr);
+      statusCode = 400;
+      resValue = stderr;
     }
     if (error !== null) {
-      res.status(500).json(error);
+      statusCode = 500;
+      resValue = error;
+
     }
+    if (stdout !== null) {
+      statusCode = 200;
+      resValue = stdout;
+    }
+    res.status(statusCode).json(resValue);
   });
 });
 
@@ -45,14 +62,21 @@ router.post('/query', function (req, res){
     searchString += '--' + req.body[i].field +'='+ req.body[i].value +' ';
   }
   child = exec('jira-miner query search ' + searchString + '--json',{maxBuffer: 1024 * 20000}, function (error, stdout, stderr) {
-    stdout = JSON.parse(stdout);
-    res.status(200).json(stdout);
     if (stderr !== null){
-      res.status(400).json(stderr);
+      statusCode = 400;
+      resValue = stderr;
     }
     if (error !== null) {
-      res.status(500).json(error);
+      statusCode = 500;
+      resValue = error;
+
     }
+    if (stdout !== null) {
+      statusCode = 200;
+      stdout = JSON.parse(stdout);
+      resValue = stdout;
+    }
+    res.status(statusCode).json(resValue);
   });
 });
 
